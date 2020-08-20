aee37dbf3d1299b441f6183fbb842e675ee47d43
Chris Nosowsky <ChrisNosowsky@gmail.com>
2020-08-19 22:00:06 -0400

    allow adding todos

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/todoitem';
+import AddTodo from './components/addTodo';
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -16,11 +17,20 @@ export default function App() {
     })
   }
 
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+      return [
+        {text: text, key: Math.random().toString()},
+        ...prevTodos
+      ]
+    })
+  }
+
   return (
     <View style={styles.container}>
       <Header></Header>
       <View style={styles.content}>
-        {/* Form */}
+        <AddTodo submitHandler={submitHandler}></AddTodo>
         <View style={styles.list}>
           <FlatList 
             data={todos}
